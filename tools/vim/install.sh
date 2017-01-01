8bcf2da69da810ea7a17f205c90cf5bf0153d954
wildbook <www762268@foxmail.com>
2017-01-02 00:23:42 +0800

    Install nvim and config SpaceVim

--- tools/vim/install.sh
+++ tools/vim/install.sh
@@ -3,6 +3,9 @@
 # First install nvim
 
 # download SpaceVim
-git clone https://github.com/SpaceVim/SpaceVim.git ~/.config/nvim
+git clone https://github.com/SpaceVim/SpaceVim.git nvim
 
 # config 环境变量
+echo "# nvim HOME">>~/.bashrc
+echo "export XDG_CONFIG_HOME=`pwd`" >>~/.bashrc
+source ~/.bashrc
