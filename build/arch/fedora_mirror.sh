b5fed6e511f4bb147d1b0571f5783b6c3aa98cff
wildbook <www762268@foxmail.com>
2017-01-02 02:00:16 +0800

    config fedora mirror

--- build/arch/fedora_mirror.sh
+++ build/arch/fedora_mirror.sh
@@ -4,3 +4,9 @@ wget -O /etc/yum.repos.d/fedora-updates.repo http://mirrors.aliyun.com/repo/fedo
 #rpmfusion
 rpm -Uvh http://mirrors.aliyun.com/rpmfusion/free/fedora/rpmfusion-free-release-25.noarch.rpm
 rpm -Uvh http://mirrors.aliyun.com/rpmfusion/nonfree/fedora/rpmfusion-nonfree-release-25.noarch.rpm
+
+#FZUG
+dnf install http://repo.fdzh.org/FZUG/free/23/x86_64/fzug-release-23-0.1.noarch.rpm
+
+#google-chrome
+wget https://repo.fdzh.org/chrome/google-chrome-mirrors.repo -P /etc/yum.repos.d/  # Fedora/RHEL  
