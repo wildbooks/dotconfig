13338b142ba2e4cdb46e93023ca55cb37985a29c
wildbook <www762268@foxmail.com>
2017-01-02 01:05:55 +0800

    config emacs

--- tools/emacs/install.sh
+++ tools/emacs/install.sh
@@ -3,6 +3,10 @@ CMD=dnf
 # First install emacs
 sudo  $CMD install emacs
 # 官方配置
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+git clone https://github.com/syl20bnr/spacemacs.git -b develop
 # 子龙山人的配置
 git clone https://github.com/zilongshanren/spacemacs-private.git
+
+#
+ln -s `pwd`/spacemacs ~/.emacs.d
+ln -s `pwd`/spacemacs-private ~/.spacemacs.d
