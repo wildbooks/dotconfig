9abad4e42b34fc705836c187035db83719c5c6b9
wildbook <www762268@foxmail.com>
2017-01-12 00:25:44 +0800

    add vimconfig for user of root

--- tools/vim/install.sh
+++ tools/vim/install.sh
@@ -1,6 +1,9 @@
 #! /usr/bin/bash
 
 CMD=dnf
+HOME=/home/`whoami`
+VIMFILES_PATH=.cache/vimfiles
+
 # First install nvim
 sudo $CMD install nvim
 
@@ -8,6 +11,13 @@ sudo $CMD install nvim
 git clone https://github.com/SpaceVim/SpaceVim.git nvim
 
 # config 环境变量
+echo "">>~/.bashrc
 echo "# nvim HOME">>~/.bashrc
 echo "export XDG_CONFIG_HOME=`pwd`" >>~/.bashrc
+
+echo "">>/root/.bashrc
+echo "# nvim HOME">>/root/.bashrc
+echo "export XDG_CONFIG_HOME=`pwd`" >>/root/.bashrc
 source ~/.bashrc
+
+sudo ln -s ${HOME}/$VIMFILES_PATH /root/${VIMFILES_PATH}
