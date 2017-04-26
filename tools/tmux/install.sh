7030ebcfaa69dfc299d66594d8772a6b778a513b
wildbook <www762268@foxmail.com>
2017-04-26 15:53:47 +0800

    添加tmux配置

---
+++ tools/tmux/install.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/bash
+
+CMD=dnf
+HOME=/home/`whoami`
+VIMFILES_PATH=.cache/vimfiles
+
+# First install nvim
+sudo $CMD install nvim
+
+# download SpaceVim
+git clone https://github.com/SpaceVim/SpaceVim.git SpaceVim
+git clone https://github.com/wildbooks/SpaceVim.d.git SpaceVim.d
+
+# user config
+ln -s `pwd`/SpaceVim  ~/.config/nvim
+ln -s `pwd`/SpaceVim.d  ~/.SpaceVim.d
+
+# root config
+suod ln -s `pwd`/SpaceVim.d  /root/.SpaceVim.d
+sudo ln -s `pwd`/SpaceVim /root/.config/nvim
+sudo ln -s ${HOME}/$VIMFILES_PATH /root/${VIMFILES_PATH}
