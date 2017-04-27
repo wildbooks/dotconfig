6e53c6b33ec7ee218a2f2cd1c058c6fdf71fd3bf
wildbook <wildbook@wildbook.lan>
2017-04-27 19:13:00 +0800

    新文件：   tools/rtorrent/install.sh 	新文件：   tools/rtorrent/rtorrent.rc

---
+++ tools/rtorrent/install.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+CMD=dnf
+HOME=/home/`whoami`
+
+# First install rtorrent 
+sudo $CMD install rtorrent
+
+# user config
+ln -s `pwd`/rtorrent.rc  ~/.rtorrent.rc
+
+# root config
+sudo ln -s `pwd`/rtorrent.rc  /root/.rtorrent.rc
