4f826cde7c380cc225082768029b200c6ac4ce26
wildbook <www762268@foxmail.com>
2017-05-28 18:18:56 +0800

    修改tmux配置

--- tools/tmux/install.sh
+++ tools/tmux/install.sh
@@ -1,14 +1,13 @@
 #! /usr/bin/bash
 
 CMD=dnf
-#HOME=/home/`whoami`
-#VIMFILES_PATH=.cache/vimfiles
 
 # First install tmux tmuxinator
 sudo $CMD install tmux tmuxinator
 
-
+git clone https://github.com/gpakosz/.tmux.git  tmux
 # user config
-#ln -s `pwd`/SpaceVim  ~/.config/nvim
-
+ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
+ln -s `pwd`/tmux/.tmux.conf.local ~/.tmux.conf.local
 # root config
+#sudo ln -s `pwd`/tmux.conf /root/.tmux.conf
