1a074ac346dfc15701dacb0d7c4cb7d6c1f1743b
wildbook <yuchun.yang@jrdcom.com>
2017-06-30 10:13:19 +0800

    新文件:       tools/shell/install.sh 	新文件:       tools/shell/shell/update.sh

---
+++ tools/shell/install.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+CMD=apt-get
+
+# First install tmux tmuxinator
+sudo $CMD install tmux tmuxinator
+
+git clone https://github.com/gpakosz/.tmux.git  tmux
+# user configs
+if [ ! -L ~/.tmux.conf ]; then #软连接文件不存在
+    rm -rf ~/.tmux.conf
+    ln -s `pwd`/tmux/.tmux.conf ~/.tmux.conf
+fi
+
+if [ ! -L ~/.emacs.d ]; then #软连接文件不存在
+    rm -rf ~/.tmux.conf.local
+    ln -s `pwd`/tmux/.tmux.conf.local ~/.tmux.conf.local
+fi
+# root config
+#sudo ln -s `pwd`/tmux.conf /root/.tmux.conf
